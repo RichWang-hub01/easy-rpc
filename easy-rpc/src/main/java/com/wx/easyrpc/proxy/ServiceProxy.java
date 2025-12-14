706be0dddc09efabde4e9aba6301c73d69e5471a
wangxin <a15194633389@163.com>
2025-12-14 19:20:49 +0800

    feat(rpc): 引入动态代理实现远程服务调用
    - 添加 ServiceProxy 类处理远程方法调用逻辑
    - 创建 ServiceProxyFactory 工厂类用于生成代理对象
    - 替换消费者示例中的静态代理为动态代理方式
    - 使用 JDK 动态代理机制完成服务接口的代理创建

---
+++ easy-rpc/src/main/java/com/wx/easyrpc/proxy/ServiceProxy.java
@@ -0,0 +1,55 @@
+package com.wx.easyrpc.proxy;
+
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import com.wx.easyrpc.model.RpcRequest;
+import com.wx.easyrpc.model.RpcResponse;
+import com.wx.easyrpc.serializer.JdkSerializer;
+import com.wx.easyrpc.serializer.Serializer;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author WX
+ * @date 2025-12-14 18:17
+ * @description 服务代理类
+ **/
+public class ServiceProxy implements InvocationHandler {
+    /**
+     * 代理逻辑
+     *
+     * @param proxy
+     * @param method
+     * @param args
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 1.创建RpcRequest
+        RpcRequest rpcRequest = RpcRequest.builder()
+                .serviceName(method.getDeclaringClass().getName())
+                .methodName(method.getName())
+                .parameterTypes(method.getParameterTypes())
+                .args(args).build();
+        // 2.指定序列化器
+        Serializer serialize = new JdkSerializer();
+        // 2.将请求序列化
+        try {
+            byte[] requestBytes = serialize.serialize(rpcRequest);
+            byte[] result;
+            // 3.发送请求
+            // todo 此处服务地址硬编码，后续使用服务注册中心
+            HttpResponse httpResponse = HttpRequest.post("http://localhost:8080")
+                    .body(requestBytes)
+                    .execute();
+            // 4.构建响应
+            result = httpResponse.bodyBytes();
+            RpcResponse rpcResponse = serialize.deserialize(result, RpcResponse.class);
+            return rpcResponse.getData();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- example-consumer/src/main/java/com/wx/example/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/wx/example/EasyConsumerExample.java
@@ -1,5 +1,6 @@
 package com.wx.example;
 
+import com.wx.easyrpc.proxy.ServiceProxyFactory;
 import com.wx.example.common.model.User;
 import com.wx.example.common.service.UserService;
 
@@ -13,7 +14,10 @@ public class EasyConsumerExample {
         User user = new User("zhangsan");
 
         // 创建服务,先使用静态代理
-        UserServiceProxy userServiceProxy = new UserServiceProxy();
+        //UserServiceProxy userServiceProxy = new UserServiceProxy();
+
+        // 使用动态代理 创建服务
+        UserService userServiceProxy = ServiceProxyFactory.getProxy(UserService.class);
         // 调用服务
         User newUser = userServiceProxy.getUser(user);
 
