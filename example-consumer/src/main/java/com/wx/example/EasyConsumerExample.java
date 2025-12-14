b6fe815bd0f630475807f84396f1064e38f1013d
wangxin <a15194633389@163.com>
2025-12-14 16:17:39 +0800

    refactor(example): - 将 UserService的空实例替换为UserServiceProxy 静态代理 实现简单的远程调用

--- example-consumer/src/main/java/com/wx/example/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/wx/example/EasyConsumerExample.java
@@ -10,12 +10,12 @@ import com.wx.example.common.service.UserService;
  **/
 public class EasyConsumerExample {
     public static void main(String[] args) {
-        // 创建服务,先指定为null
-        UserService userService = null;
         User user = new User("zhangsan");
 
+        // 创建服务,先使用静态代理
+        UserServiceProxy userServiceProxy = new UserServiceProxy();
         // 调用服务
-        User newUser = userService.getUser(user);
+        User newUser = userServiceProxy.getUser(user);
 
         if (newUser != null){
             System.out.println("用户名" + newUser.getName());
